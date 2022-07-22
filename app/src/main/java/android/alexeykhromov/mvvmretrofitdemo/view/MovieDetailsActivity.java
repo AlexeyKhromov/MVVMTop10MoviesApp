dbf1a050dfa4b51b490aabf881201b3c6e2176a4
Alexey <Lelik8888@gmail.com>
2022-07-22 12:30:32 +0300

    Create MovieDetailsActivity logic

--- app/src/main/java/android/alexeykhromov/mvvmretrofitdemo/view/MovieDetailsActivity.java
+++ app/src/main/java/android/alexeykhromov/mvvmretrofitdemo/view/MovieDetailsActivity.java
@@ -7,16 +7,30 @@ import android.alexeykhromov.mvvmretrofitdemo.R;
 import android.alexeykhromov.mvvmretrofitdemo.model.Result;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
     private Result result;
+    private ImageView posterImageView;
+    private  String posterPath;
+    private TextView titleTextVIew;
+    private TextView voteCountTextView;
+    private TextView overviewTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        posterImageView = findViewById(R.id.posterImageView);
+        titleTextVIew = findViewById(R.id.titleTextView);
+        voteCountTextView = findViewById(R.id.voteCountTextView);
+        overviewTextView = findViewById(R.id.overviewTextView);
 
         Intent intent = getIntent();
 
@@ -24,6 +38,14 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
             result = intent.getParcelableExtra("movieData");
             Toast.makeText(this, result.getOriginalTitle(), Toast.LENGTH_SHORT).show();
+
+            posterPath = result.getPosterPath();
+            String imagePath = "https://image.tmdb.org/t/p/w500/" + posterPath;
+            Glide.with(this).load(imagePath).placeholder(R.drawable.circle_progress).into(posterImageView);
+
+            titleTextVIew.setText(result.getTitle());
+            voteCountTextView.setText(Integer.toString(result.getVoteCount()));
+            overviewTextView.setText(result.getOverview());
         }
     }
 }
